699e7e2da4bf9f6854daf22634b9259993057cd9
LA\bermudez.j <jose.92.b@hotmail.com>
2017-03-24 20:52:48 -0600

    first commit

---
+++ Examen2/Examen2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Examen2.Startup))]
+namespace Examen2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
